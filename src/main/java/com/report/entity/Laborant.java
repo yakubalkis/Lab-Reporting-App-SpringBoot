d3808d29982e3561a35f17dc2f5a6fbb06fbae64
yakubalkis <alkis.yakub@gmail.com>
2023-05-09 19:24:08 +0300

    Created Laborant class.

---
+++ src/main/java/com/report/entity/Laborant.java
@@ -0,0 +1,82 @@
+package com.report.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "laborant")
+public class Laborant {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "id_no")
+    private String idNo;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "laborant_id")
+    private List<Report> reports;
+
+    public List<Report> getReports() {
+        return reports;
+    }
+
+    public void setReports(List<Report> reports) {
+        this.reports = reports;
+    }
+    // add a convenience method
+    public void addReport(Report report){
+        if(reports == null){
+            reports = new ArrayList<>();
+        }
+        reports.add(report);
+    }
+
+    public Laborant() {
+    }
+
+    public Laborant(String firstName, String lastName, String idNo) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNo = idNo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+}

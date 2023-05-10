807b86366a7aa2bc6d52cef8594c22c012a79404
yakubalkis <alkis.yakub@gmail.com>
2023-05-10 22:02:44 +0300

    Added findByIdNo method for login process.

--- src/main/java/com/report/repository/LaborantRepository.java
+++ src/main/java/com/report/repository/LaborantRepository.java
@@ -4,5 +4,5 @@ import com.report.entity.Laborant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LaborantRepository extends JpaRepository<Laborant, Integer> {
-
+        Laborant findByIdNo(String idNo);
 }

37d53845b87b724e1f545537c1f8991c28d60b0e
yakubalkis <alkis.yakub@gmail.com>
2023-05-11 21:36:58 +0300

    Added overrided methods for UserDetailsService.

--- src/main/java/com/report/service/LaborantServiceImpl.java
+++ src/main/java/com/report/service/LaborantServiceImpl.java
@@ -1,20 +1,31 @@
 package com.report.service;
 
 import com.report.entity.Laborant;
+import com.report.entity.Role;
 import com.report.repository.LaborantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class LaborantServiceImpl implements LaborantService{
 
     private LaborantRepository laborantRepository;
+    private BCryptPasswordEncoder passwordEncoder;
     @Autowired
-    public LaborantServiceImpl(LaborantRepository laborantRepository) {
+    public LaborantServiceImpl(LaborantRepository laborantRepository, @Lazy BCryptPasswordEncoder passwordEncoder) {
         this.laborantRepository = laborantRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -40,12 +51,32 @@ public class LaborantServiceImpl implements LaborantService{
     }
 
     @Override
-    public void save(Laborant laborant) {
-        laborantRepository.save(laborant);
+    public Laborant save(Laborant laborant) {
+       String password = laborant.getPassword();
+       String encodedPassword = passwordEncoder.encode(password);
+       laborant.setPassword(encodedPassword);
+
+        return laborantRepository.save(laborant);
     }
 
     @Override
     public void deleteById(Integer id) {
         laborantRepository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Laborant laborant = laborantRepository.findByIdNo(username);
+        if(laborant==null){
+            throw new UsernameNotFoundException("Invalid username or password!");
+        }
+
+        return new org.springframework.security.core.userdetails.User(laborant.getIdNo(), laborant.getPassword(), mapRolesToAuthorities(laborant.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }

94e8bb6bb2baef184553b933d46e285f083e6b50
yakubalkis <alkis.yakub@gmail.com>
2023-05-12 16:40:11 +0300

    Added custom query method findLaborantByIdNo.

--- src/main/java/com/report/repository/LaborantRepository.java
+++ src/main/java/com/report/repository/LaborantRepository.java
@@ -2,7 +2,11 @@ package com.report.repository;
 
 import com.report.entity.Laborant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LaborantRepository extends JpaRepository<Laborant, Integer> {
         Laborant findByIdNo(String idNo);
+
+        @Query("SELECT lab FROM Laborant lab WHERE lab.idNo = ?1")
+        Laborant findLaborantByIdNo(String idNo);
 }
